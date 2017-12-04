120df9c0f707ed623e70e96a9efd287693cb3f03
Valerii Iatsko <viatsko@viatsko.me>
2017-12-04 22:22:03 +0100

    v0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # google-cloud-datastore-node
+
 NodeJS library to simplify working with Google Cloud DataStore
+
+Based on a code by Google https://github.com/GoogleCloudPlatform/nodejs-getting-started
+
+The difference is that this library uses Promises instead of callbacks which leads to a better code organization
+and can be used as well with async/await.
+
+## Usage
+
+### Setting up project id
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+setup({
+    projectId: 'my-awesome-project'
+});
+```
+
+### Creating an entity
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+const testDao = createDao('Test');
+
+testDao.create({
+    firstname: 'John',
+    lastname: 'Tester'
+}).then(() => {
+    console.log('Entry has been created!');
+}).catch(err => console.error(err));
+```
+
+### Reading an entity
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+const testDao = createDao('Test');
+
+testDao.read('<ID>').then((data) => {
+    console.log(data);
+}).catch(err => console.error(err));
+```
+
+### Updating an entity
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+const testDao = createDao('Test');
+
+testDao.update('<ID>', {
+    firstname: 'John',
+    lastname: 'Tester'
+}).then(() => {
+    console.log('Entry has been updated!');
+}).catch(err => console.error(err));
+```
+
+### Deleting an entity
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+const testDao = createDao('Test');
+
+testDao.delete('<ID>').then(() => {
+    console.log('Entry has been deleted!');
+});
+```
+
+### Listing entities
+
+```js
+const { setup, createDao } = require('google-cloud-datastore-node');
+
+const testDao = createDao('Test');
+
+/* parameters are `limit`, `order`, `token` */
+testDao.list(10, 'test').then(({ data, hasMore }) => {
+    /* `hasMore` can be re-used as a `token` param in case of pagination implementation */
+    console.log(data);
+});
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+const Datastore = require('@google-cloud/datastore');
+
+let projectId = '';
+
+function fromDatastore(obj) {
+    obj.id = obj[Datastore.KEY].id;
+
+    return obj;
+}
+
+function toDatastore(obj, nonIndexed) {
+    nonIndexed = nonIndexed || [];
+
+    const results = [];
+
+    Object.keys(obj).forEach((k) => {
+        if (obj[k] === undefined) {
+            return;
+        }
+        results.push({
+            name: k,
+            value: obj[k],
+            excludeFromIndexes: nonIndexed.indexOf(k) !== -1
+        });
+    });
+
+    return results;
+}
+
+function setup(options) {
+    if (typeof options !== 'object') {
+        throw new Error('`options` should be an object.')
+    }
+
+    projectId = options.projectId;
+}
+
+function createDao(daoProjectId, entityName) {
+    if (!entityName) {
+        entityName = daoProjectId;
+    } else {
+        projectId = daoProjectId;
+    }
+
+    const ds = new Datastore({
+        projectId
+    });
+
+    function list(limit, order, token) {
+        return new Promise((resolve, reject) => {
+            const q = ds.createQuery([entityName]);
+
+            if (limit) {
+                q.limit(limit);
+            }
+
+            if (order) {
+                q.order(order);
+            }
+
+            if (token) {
+                q.start(token);
+            }
+
+            ds.runQuery(q, (err, entities, nextQuery) => {
+                if (err) {
+                    reject(err);
+
+                    return;
+                }
+
+                const hasMore = nextQuery.moreResults !== Datastore.NO_MORE_RESULTS ? nextQuery.endCursor : false;
+
+                resolve({
+                    data: entities.map(fromDatastore),
+                    hasMore,
+                });
+            });
+        });
+    }
+
+    function update(id, data) {
+        return new Promise((resolve, reject) => {
+            let key;
+
+            if (id) {
+                key = ds.key([entityName, parseInt(id, 10)]);
+            } else {
+                key = ds.key(entityName);
+            }
+
+            const entity = {
+                key: key,
+                data: toDatastore(data, ['description']),
+            };
+
+            ds.save(
+                entity,
+                (err) => {
+                    data.id = entity.key.id;
+
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                }
+            );
+        });
+    }
+
+    function create(data) {
+        return update(null, data);
+    }
+
+    function read(id) {
+        return new Promise((resolve, reject) => {
+            const key = ds.key([entityName, parseInt(id, 10)]);
+
+            ds.get(key, (err, entity) => {
+                if (!err && !entity) {
+                    err = {
+                        code: 404,
+                        message: 'Not found'
+                    };
+                }
+
+                if (err) {
+                    reject(err);
+
+                    return;
+                }
+
+                resolve(fromDatastore(entity));
+            });
+        });
+    }
+
+    function _delete(id) {
+        return new Promise((resolve, reject) => {
+            const key = ds.key([entityName, parseInt(id, 10)]);
+
+            ds.delete(key, resolve);
+        });
+    }
+
+    return {
+        create,
+        read,
+        update,
+        delete: _delete,
+        list
+    };
+}
+
+module.exports = {
+    setup,
+    createDao,
+};
